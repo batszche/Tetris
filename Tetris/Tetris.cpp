7ffc911f03455aeec84ffb8676994264c8b9e7b0
bat <35456642+batszche@users.noreply.github.com>
2026-02-11 00:16:51 +0900

    260211

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -2,6 +2,35 @@
 #include "Tetris.h"
 using namespace std;
 
-class TetrisBoard {
+Tetris::Tetris() {
+	// 벽 배열 초기화// position curPos = { 0, 0 }; // 주의: 여기서 새로 선언하면 멤버 변수가 아니라 '지역 변수'가 됩니다.
+    // curPos = { 5, 0 }; // 헤더에 선언된 멤버 변수를 그대로 쓰세요.
+	for (int i = 0; i < 12; i++) {
+		for (int j = 0; j < 22; j++) {
+			if (i == 0 || i == 11 || j == 21)
+			{
+				BOARD[i][j] = 1;
+			}
+			else BOARD[i][j] = 0;
+		}
+	}
 
-};
+	// 변수 세팅: 점수 및 블록 위치
+
+	curPos = { 0, 0 };
+	score = 0;
+}
+
+void Tetris::draw() {
+	system("cls");
+
+	for (int i = 0; i < 12; i++) {
+		for (int j = 0; j < 22; j++) {
+			if (i == 0 || i == 11 || j == 21)
+			{
+				BOARD[i][j] = 1;
+			}
+			else BOARD[i][j] = 0;
+		}
+	}
+}
--- Tetris/Tetris.h
+++ Tetris/Tetris.h
@@ -1,5 +1,29 @@
 #pragma once
 
 class Tetris {
-
+private:
+	// 총 점수
+	int score;
+	// 보드의 크기
+	int BOARD[22][12];
+	// 현재 블록의 좌표
+	struct position { int x, y; };
+	position curPos;
+public:
+	// 생성자
+	Tetris();
+	// 소멸자
+	~Tetris();
+	// 테트리스 보드 그리기
+	void draw();
+	// 열 지우기
+	void lineClear();
+	// 블록 이동
+	void blockMove(int dx, int dy);
+	// 블록 고정
+	void blockFix();
+	// 외부에서 점수 값을 수정
+	void setScore(int score);
+	// 외부에서 점수 값에 접근
+	int getScore();
 };
